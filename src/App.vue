9efecaaa42625633359ee9bae26638329f39ee73
SmitnAntonyDv <anton.smit@hotmail.com>
2020-08-30 15:00:29 +0200

    added todos data

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
 <template>
-  <div id="app">
-    
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {
-   
+  components: {},
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Morning priming",
+          completion: true,
+        },
+        {
+          id: 2,
+          title: "Practicing algorithms and data structures",
+          completion: false,
+        },
+        {
+          id: 3,
+          title: "Create and work on new Websocket project",
+          completion: false,
+        },
+        {
+          id: 4,
+          title: "Gym",
+          completion: false,
+        },
+      ],
+    };
   },
 };
 </script>
