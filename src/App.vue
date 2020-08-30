2e2ed9fbed12e596f62e08da554ca1a3531521c8
SmitnAntonyDv <anton.smit@hotmail.com>
2020-08-30 18:27:39 +0200

    added newTodo functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
+    Header,
     Todos,
+    AddTodo,
   },
 
   data() {
@@ -18,22 +24,22 @@ export default {
       todos: [
         {
           id: 1,
-          title: "Morning priming",
+          title: 'Morning priming',
           completed: true,
         },
         {
           id: 2,
-          title: "Practicing algorithms and data structures",
+          title: 'Practicing algorithms and data structures',
           completed: false,
         },
         {
           id: 3,
-          title: "Create and work on new Websocket project",
+          title: 'Create and work on new Websocket project',
           completed: false,
         },
         {
           id: 4,
-          title: "Gym",
+          title: 'Gym',
           completed: false,
         },
       ],
@@ -43,9 +49,29 @@ export default {
     deleteTodo(id) {
       this.todos = this.todos.filter((todo) => todo.id !== id);
     },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
   },
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
 </style>
