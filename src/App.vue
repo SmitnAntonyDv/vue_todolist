6e75104a009c80598797a6bfc4415dcd376090c4
SmitnAntonyDv <anton.smit@hotmail.com>
2020-08-30 19:23:24 +0200

    added vue-router functionality -> simple HOME page and ABOUT page

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,40 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
-
 <script>
 import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import axios from 'axios';
-
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     Header,
-    Todos,
-    AddTodo,
-  },
-
-  data() {
-    return {
-      todos: [],
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-
-      axios
-        .post(`https://jsonplaceholder.typicode.com/posts`, {
-          title,
-          completed,
-        })
-        .then((res) => (this.todos = [...this.todos, res.data]))
-        .catch((err) => console.log('error!', err));
-    },
-  },
-  created() {
-    axios
-      .get(`https://jsonplaceholder.typicode.com/todos?_limit=5`)
-      .then((res) => (this.todos = res.data))
-      .catch((err) => console.log('error!', err));
   },
 };
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
+#nav {
+  padding: 30px;
 }
-.btn:hover {
-  background: #666;
+
+#nav a {
+  font-weight: bold;
+  color: white;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: white;
+  text-decoration: none;
 }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a Todo-list App. A simple app to play around with Vue.Js</p>
+    <p>
+      This App utilizes basic vue concepts and syntax as well as the router
+      functionality using Vue-CLI UI
+    </p>
+  </div>
+</template>
